743855f56c3b43479c998c36a62ce2a0080ad49f
Edmond Chuc <edmond.s.chuc@gmail.com>
2018-04-27 15:16:47 +1000

    Servlet function to add namespaces to namespaces.txt

---
+++ src/main/java/com/edmondchuc/lode2/Add.java
@@ -0,0 +1,66 @@
+package com.edmondchuc.lode2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Add
+ */
+//@WebServlet("/Add")
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String namespace = request.getParameter("namespace");
+		
+		// get the directory of this application
+		String path = System.getProperty("user.dir");
+		
+		// path to the text document of namespaces
+		String filePath = Paths.get(path + File.separator + "src/main/webapp/namespaces.txt").toString();
+	
+		FileWriter fileWriter = new FileWriter(filePath, true);
+		
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		
+		bufferedWriter.append("\n" + namespace + "\n");
+		
+		bufferedWriter.close();
+		log("Namespace " + namespace + " added to namespace.txt.");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
